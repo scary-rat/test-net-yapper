11a62565d2d7ed531622114d5704c9f5fd1fc6c3
Scary Rat <verceldeploys@proton.me>
2024-09-30 00:57:13 +0530

    Update generateCookieAndSave.js

--- backend/src/utils/helpers/generateCookieAndSave.js
+++ backend/src/utils/helpers/generateCookieAndSave.js
@@ -11,7 +11,8 @@ const generateCookieAndSave = (userId, res) => {
         // yo chai more secure hunxa re, httpOnly slie true garyo vaney browser ko javascript le tyo cookie lie access garna sakdaina re
         httpOnly: true,
         maxAge: 15 * 24 * 60 * 60 * 1000, //yes ma directly "15d" lekhna mildaina, hamile ms ma time dinu parxa
-        sameSite: "strict" // yo pani security ko lagi ho re, CSRF attack lie prevent garna re
+        sameSite: "strict", // yo pani security ko lagi ho re, CSRF attack lie prevent garna re
+        secure: process.env.NODE_ENV === 'production' // Set to true in production
     })
     console.log("cookie created and saved")
 
